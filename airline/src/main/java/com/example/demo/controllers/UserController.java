915f62c74c5ab1a38fab2a12d855001f1e8d2c52
mrunalipardhi2000 <mrunali30pardhi@gmail.com>
2025-01-29 16:46:16 +0530

    softdelete of user,update user,getall user

--- airline/src/main/java/com/example/demo/controllers/UserController.java
+++ airline/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.ApiResponse;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.UserDTO;
+import com.example.demo.dto.UserRespDTO;
+import com.example.demo.entity.UserEntity;
 import com.example.demo.services.UserService;
 
 @RestController
@@ -41,5 +48,34 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(userService.addUser(user));
 	}
+    
+    
+    //update user 
+    @PutMapping("/{userId}")
+    public ResponseEntity<?> updateUserDetails(@PathVariable Long userId,@RequestBody UserDTO user)
+    {
+    	System.out.println("in update user " + userId + " " + user);
+    
+    return ResponseEntity.ok(userService.updateUser(userId, user));
+    }
+    
+    
+    @PutMapping("/{userId}/deactivate")
+    public ResponseEntity<ApiResponse> deactivateUser(@PathVariable Long userId) {
+        ApiResponse response = userService.softDeleteUser(userId);
+        return ResponseEntity.ok(response);
+    }
+   
 	
+    
+   	@GetMapping
+   	public ResponseEntity<?> getUser() {
+   		
+   		List<UserRespDTO> user = 
+   				userService.getAllUser();
+   		if (user.isEmpty())
+   			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+   		return ResponseEntity.ok(user);
+   	}
+    
 }
