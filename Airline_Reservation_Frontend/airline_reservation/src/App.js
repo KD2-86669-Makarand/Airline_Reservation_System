ff9f82d355575362c467013e38008ad7dba34422
guest <76803160+adityakumar293@users.noreply.github.com>
2025-02-10 13:59:41 +0530

    New One

--- Airline_Reservation_Frontend/airline_reservation/src/App.js
+++ Airline_Reservation_Frontend/airline_reservation/src/App.js
@@ -1,29 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Login from "./screens/login";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./screens/home";
+import Login from "./screens/login";
 import Register from "./screens/Register";
 import SearchFlight from "./screens/SearchFlight";
 import AvailableFlights from "./screens/AvailableFlights";
 import AddAirline from "./screens/AddAirlines";
 import AdminHome from "./screens/Adminhome";
 import UserList from "./screens/Users";
-import BookFlight from "./screens/Book";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="Register" element={<Register />} />
-        {/* <Route path="SearchFlight" element={<SearchFlight />} /> */}
-        <Route path="AvailableFlights" element={<AvailableFlights />} />
-        <Route path="Book" element={<BookFlight />} />
-        <Route path="AddAirlines" element={<AddAirline />} />
-        <Route path="AdminHome" element={<AdminHome />} />
-        <Route path="UserList" element={<UserList/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/search-flight" element={<SearchFlight />} />
+        <Route path="/available-flights" element={<AvailableFlights />} />
+        <Route path="/add-airlines" element={<AddAirline />} />
+        <Route path="/admin-home" element={<AdminHome />} />
+        <Route path="/user-list" element={<UserList />} />
       </Routes>
     </div>
   );
